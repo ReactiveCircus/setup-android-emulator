de2192afd2f73470aa4e4a74f27e569ca0a1fc68
Yang Chen <ychescale9@gmail.com>
2019-11-03 15:41:35 +1100

    Update script to support upcoming emulator-headless retirement.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ $ANDROID_HOME/tools/bin/sdkmanager "system-images;android-${API_LEVEL};${IMG_TYP
 
 # Create a new instance of AVD and start the emulator headless mode
 $ANDROID_HOME/tools/bin/avdmanager create avd --force --name "api-${API_LEVEL}" --abi "${IMG_TYPE}/${SYS_IMG}" --package "system-images;android-${API_LEVEL};${IMG_TYPE};${SYS_IMG}" --device "Nexus 6P"
-$ANDROID_HOME/emulator/emulator-headless -avd "api-${API_LEVEL}" -gpu swiftshader -no-snapshot -noaudio -no-window -no-boot-anim &
+$ANDROID_HOME/emulator/emulator -avd "api-${API_LEVEL}" -gpu swiftshader -no-window -no-snapshot -noaudio -no-boot-anim &
 $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
 $ANDROID_HOME/platform-tools/adb shell settings put global window_animation_scale 0.0
 $ANDROID_HOME/platform-tools/adb shell settings put global transition_animation_scale 0.0
